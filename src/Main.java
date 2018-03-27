0112409451e4a4b978eec074823c493fff804ec2
brunork26 <bruno.r.kieling@hotmail.com>
2018-03-27 20:57:46 -0300

    Tretas

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by brunork on 27/03/18.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Bar BarDoDibre = new Bar();
+        BarDoDibre.abrirBar();
+    }
+}
